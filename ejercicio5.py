dbf0b2df160ebb28fbb818daa5c925741f237767
msansonetti <127798643+msansonetti@users.noreply.github.com>
2023-03-27 23:10:46 +0200

    Delete ejercicio5.py

--- ejercicio5.py
+++
@@ -1,9 +0,0 @@
-def aniobisiesto():
-    anio = int(input("Introduzca un año para saber si es o no bisiesto: "))
-    if anio % 4 == 0 and anio % 100 != 0 or anio % 400 == 0:
-        print("El año: ", anio, "SI es bisiesto.")
-    else:
-        print("El año: ", anio, "NO es bisiesto.")
-
-aniobisiesto()
-
